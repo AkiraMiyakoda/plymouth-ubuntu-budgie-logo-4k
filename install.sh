9c05dd198179523434abea4cde8e299c71dbab1c
Akira Miyakoda <akira.miyakoda@gmail.com>
2021-06-24 01:02:26 +0900

    Remove uselsss sudo.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Install theme
-sudo cp -r ./ubuntu-budgie-logo-4k/ /usr/share/plymouth/themes/
+cp -r ./ubuntu-budgie-logo-4k/ /usr/share/plymouth/themes/
 update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/ubuntu-budgie-logo-4k/ubuntu-budgie-logo-4k.plymouth 150
 update-alternatives --config default.plymouth
 update-initramfs -u
